5c50fb159739ae12fd44a52283d6d0d0be6b87ee
Enamul Haque <ehaque95@gmail.com>
2017-07-03 16:02:59 +0600

    date Picker
    Swift 3 date Picker

---
+++ DatePicker1/ViewController.swift
@@ -0,0 +1,49 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var txtDatePicker: UITextField!
+    
+    let datePicker = UIDatePicker()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showDatePicker()
+        
+    }
+    
+    
+    func showDatePicker(){
+        //Formate Date
+        datePicker.datePickerMode = .date
+        
+        //ToolBar
+        let toolbar = UIToolbar();
+        toolbar.sizeToFit()
+        
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.bordered, target: self, action: "donedatePicker")
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.bordered, target: self, action: "cancelDatePicker")
+        toolbar.setItems([doneButton,spaceButton,cancelButton], animated: false)
+        
+        txtDatePicker.inputAccessoryView = toolbar
+        txtDatePicker.inputView = datePicker
+        
+    }
+    
+    func donedatePicker(){
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        txtDatePicker.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    func cancelDatePicker(){
+        self.view.endEditing(true)
+    }
+}
+
